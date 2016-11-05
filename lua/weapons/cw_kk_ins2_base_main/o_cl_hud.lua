35237b85b014b4c583f1ca90488df37d14412b1b
Ľubor Slušný <lubor.slusny@gmail.com>
2016-11-05 19:00:09 +0100

    - selecticon alpha - removed nambu

--- lua/weapons/cw_kk_ins2_base_main/o_cl_hud.lua
+++ lua/weapons/cw_kk_ins2_base_main/o_cl_hud.lua
@@ -395,12 +395,50 @@ function SWEP:drawCustomHUD()
 	end
 end
 
+local simpleTextColor = Color(255, 210, 0, 255)
+
 function SWEP:DrawWeaponSelection(x, y, wide, tall, alpha)
 	if self.SelectIcon then
-		surface.SetDrawColor(255, 255, 255, 255)
+		-- surface.SetDrawColor(255, 210, 0, 255)
+		-- surface.DrawRect(x + tall * 0.2, y + tall / 4, tall, tall / 2)
+		-- surface.DrawRect(x, y, wide, tall)
+		
+		surface.SetDrawColor(255, 255, 255, alpha)
 		surface.SetTexture(self.SelectIcon)
+		
 		surface.DrawTexturedRect(x + tall * 0.2, y + tall / 4, tall, tall / 2)
+		-- surface.DrawTexturedRect(x, y + wide / 8, wide, wide / 2)
+		
+		-- self:PrintWeaponInfo(x + wide + 20, y + tall * 0.95, alpha)
 	else
-		draw.SimpleText(self.IconLetter, self.SelectFont, x + wide / 2, y + tall * 0.2, Color(255, 210, 0, alpha), TEXT_ALIGN_CENTER)
+		simpleTextColor.a = alpha
+		draw.SimpleText(self.IconLetter, self.SelectFont, x + wide / 2, y + tall * 0.2, simpleTextColor, TEXT_ALIGN_CENTER)
 	end
+end
+
+function SWEP:PrintWeaponInfo(x, y, alpha)
+	if (self.DrawWeaponInfoBox == false) then return end
+
+	if (self.InfoMarkup == nil) then
+		local str
+		local title_color = "<color=230,230,230,255>"
+		local text_color = "<color=150,150,150,255>"
+		
+		str = "<font=HudSelectionText>"
+		if (self.Author != "") then str = str .. title_color .. "Author:</color>\t"..text_color..self.Author.."</color>\n" end
+		if (self.Contact != "") then str = str .. title_color .. "Contact:</color>\t"..text_color..self.Contact.."</color>\n\n" end
+		if (self.Purpose != "") then str = str .. title_color .. "Purpose:</color>\n"..text_color..self.Purpose.."</color>\n\n" end
+		if (self.Instructions != "") then str = str .. title_color .. "Instructions:</color>\n"..text_color..self.Instructions.."</color>\n" end
+		str = str .. "</font>"
+		
+		self.InfoMarkup = markup.Parse(str, 250)
+	end
+	
+	surface.SetDrawColor(60, 60, 60, alpha)
+	surface.SetTexture(self.SpeechBubbleLid)
+	
+	surface.DrawTexturedRect(x, y - 64 - 5, 128, 64) 
+	draw.RoundedBox(8, x - 5, y - 6, 260, self.InfoMarkup:GetHeight() + 18, Color(60, 60, 60, alpha))
+	
+	self.InfoMarkup:Draw(x+5, y+5, nil, nil, alpha)
 end
