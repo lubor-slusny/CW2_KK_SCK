046fdba4b39a60be5a010a653809e0070f6822ca
Ľubor Slušný <lubor.slusny@gmail.com>
2017-02-26 17:08:25 +0100

    g19, nam melee, nam nade, nam sterling

---
+++ lua/weapons/cw_kk_ins2_cstm_g19/sh_soundscript.lua
@@ -0,0 +1,73 @@
+
+SWEP.Sounds = {
+	base_ready = {
+		{time = 0/30, sound = "CW_KK_INS2_UNIVERSAL_PISTOL_DRAW"},
+		{time = 4/30, sound = "CW_KK_INS2_MAKAROV_SAFETY"},
+		{time = 14/30, sound = "CW_KK_INS2_CSTM_G19_BOLTBACK"},
+		{time = 20/30, sound = "CW_KK_INS2_CSTM_G19_BOLTRELEASE"},
+	},
+
+	base_draw = {
+		{time = 0/30, sound = "CW_KK_INS2_UNIVERSAL_PISTOL_DRAW"},
+	},
+
+	base_holster = {
+		{time = 0/30, sound = "CW_KK_INS2_UNIVERSAL_PISTOL_HOLSTER"},
+	},
+
+	base_crawl = {
+		{time = 0/35, sound = "CW_KK_INS2_UNIVERSAL_LEFTCRAWL"},
+		{time = 22/35, sound = "CW_KK_INS2_UNIVERSAL_RIGHTCRAWL"},
+	},
+
+	base_dryfire = {
+		{time = 0/30, sound = "CW_KK_INS2_MAKAROV_EMPTY"},
+	},
+
+	base_reload = {
+		{time = 0/30, sound = "CW_KK_INS2_CSTM_G19_MAGRELEASE"},
+		{time = 7/30, sound = "CW_KK_INS2_CSTM_G19_MAGOUT"},
+		{time = 35/30, sound = "CW_KK_INS2_CSTM_G19_MAGIN"},
+		{time = 58/30, sound = "CW_KK_INS2_MAKAROV_MAGHIT"},
+	},
+
+	base_reloadempty = {
+		{time = 0/30, sound = "CW_KK_INS2_CSTM_G19_MAGRELEASE"},
+		{time = 7/30, sound = "CW_KK_INS2_CSTM_G19_MAGOUT"},
+		{time = 35/30, sound = "CW_KK_INS2_CSTM_G19_MAGIN"},
+		{time = 58/30, sound = "CW_KK_INS2_MAKAROV_MAGHIT"},
+		{time = 71/30, sound = "CW_KK_INS2_CSTM_G19_BOLTRELEASE"},
+	},
+
+	base_reload_extmag = {
+		{time = 0/30, sound = "CW_KK_INS2_CSTM_G19_MAGRELEASE"},
+		{time = 7/30, sound = "CW_KK_INS2_CSTM_G19_MAGOUT"},
+		{time = 35/30, sound = "CW_KK_INS2_CSTM_G19_MAGIN"},
+		{time = 58/30, sound = "CW_KK_INS2_MAKAROV_MAGHIT"},
+	},
+
+	base_reloadempty_extmag = {
+		{time = 0/30, sound = "CW_KK_INS2_CSTM_G19_MAGRELEASE"},
+		{time = 7/30, sound = "CW_KK_INS2_CSTM_G19_MAGOUT"},
+		{time = 35/30, sound = "CW_KK_INS2_CSTM_G19_MAGIN"},
+		{time = 58/30, sound = "CW_KK_INS2_MAKAROV_MAGHIT"},
+		{time = 71/30, sound = "CW_KK_INS2_CSTM_G19_BOLTRELEASE"},
+	},
+
+	empty_draw = {
+		{time = 0/30, sound = "CW_KK_INS2_UNIVERSAL_PISTOL_DRAW"},
+	},
+
+	empty_holster = {
+		{time = 0/30, sound = "CW_KK_INS2_UNIVERSAL_PISTOL_HOLSTER"},
+	},
+	
+	empty_crawl = {
+		{time = 0/35, sound = "CW_KK_INS2_UNIVERSAL_LEFTCRAWL"},
+		{time = 22/35, sound = "CW_KK_INS2_UNIVERSAL_RIGHTCRAWL"},
+	},
+
+	iron_dryfire = {
+		{time = 0/30, sound = "CW_KK_INS2_MAKAROV_EMPTY"},
+	},
+}
