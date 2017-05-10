ded0bb39a5cd4fb2a49afd92415b0baeb1339d67
Ľubor Slušný <lubor.slusny@gmail.com>
2017-05-10 14:01:43 +0200

    added barebones of ports for Aimpos Builder, CVMT Panel, Element Browser and VM Freeze

---
+++ lua/cw_kk/sck/tools/vmfreeze.lua
@@ -0,0 +1,36 @@
+AddCSLuaFile()
+
+local TOOL = {}
+
+TOOL.Name = "vmfreeze"
+TOOL.PrintName = "VM Freeze 1.1"
+
+function TOOL:_updatePanel()
+	local panel = self._panel
+	local wep = self._wep
+	
+	if !IsValid(panel) then return end
+	
+	panel:ClearControls()
+	
+	if !IsValid(wep) or !wep.CW20Weapon then
+		panel:AddControl("Label", {Text = "Not a CW2 swep, move along."})
+		return
+	end
+	
+	panel:AddControl("Label", {Text = "2doo"})
+end
+
+function TOOL:SetPanel(panel)
+	self._panel = panel
+	self:_updatePanel()
+end
+
+function TOOL:OnWeaponChanged(new, old)
+	self._wep = new
+	self:_updatePanel()
+end
+
+CustomizableWeaponry_KK.sck:AddTool(TOOL)
+
+TOOL:_updatePanel()
