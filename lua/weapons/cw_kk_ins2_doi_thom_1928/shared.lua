e95c567af5ec03143e9d9f7cb8da4287eba04ae2
Ľubor Slušný <lubor.slusny@gmail.com>
2017-06-01 23:12:11 +0200

    removed sling from M1928

--- lua/weapons/cw_kk_ins2_doi_thom_1928/shared.lua
+++ lua/weapons/cw_kk_ins2_doi_thom_1928/shared.lua
@@ -85,7 +85,7 @@ SWEP.Attachments = {
 	-- {header = "Barrel", offset = {-200, -500}, atts = {"kk_ins2_ww2_knife"}},
 	{header = "Under", offset = {-500, 0}, atts = {"kk_ins2_vertgrip"}},
 	{header = "Magazine", offset = {-50, 500}, atts = {"kk_ins2_mag_thom_30", "kk_ins2_mag_thom_50"}},
-	{header = "Stock", offset = {1000, 0}, atts = {"kk_ins2_ww2_sling"}},
+	-- {header = "Stock", offset = {1000, 0}, atts = {"kk_ins2_ww2_sling"}},
 	["+reload"] = {header = "Ammo", offset = {900, 500}, atts = {"am_magnum", "am_matchgrade"}}
 }
 
