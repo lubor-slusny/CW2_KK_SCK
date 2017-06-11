56c2f15c26b981b815da90dcfd53c1706b43d8f4
Ľubor Slušný <lubor.slusny@gmail.com>
2017-06-12 00:50:11 +0200

    aimpos builder 5 draft

--- lua/cw_kk/sck/tools/aimpos.lua
+++ lua/cw_kk/sck/tools/aimpos.lua
@@ -6,6 +6,46 @@ TOOL.Name = "aimpos"
 TOOL.PrintName = "AimPos Builder"
 TOOL.Version = "5.0"
 
+function TOOL:Initialize()
+	if not self._relevantAttsCache then
+		local primary = {}
+		local secondary = {}
+		local grenade = {}
+		
+		for _,att in pairs(CustomizableWeaponry.registeredAttachments) do
+			local attInfo = {}
+			local subCache
+			
+			if att.isSight then
+				subCache = primary
+			elseif att.isSecondarySight then
+				subCache = secondary
+			elseif att.isGrenadeLauncher then
+				attInfo.prefix = "M203"
+				subCache = grenade
+			else
+				continue
+			end
+			
+			attInfo.name = att.name
+			attInfo.displayName = att.displayName
+			attInfo.prefix = attInfo.prefix
+				or att.aimPos and
+					string.sub(att.aimPos[1], 1, string.len(att.aimPos[1]) - 3)
+				or "Aim"
+			
+			table.insert(subCache, attInfo)
+		end
+		
+		self._relevantAttsCache = {}
+		self._relevantAttsCache[1] = secondary
+		self._relevantAttsCache[2] = grenade
+		self._relevantAttsCache[3] = primary		
+	end
+	
+	
+end
+
 function TOOL:_updatePanel()
 	local panel = self._panel
 	local wep = self._wep
@@ -19,7 +59,31 @@ function TOOL:_updatePanel()
 		return
 	end
 	
-	panel:AddControl("Label", {Text = "2doo"})
+	panel:AddControl("Label", {Text = "classname"})
+	panel:AddControl("Label", {Text = "cvar force xh"})
+	panel:AddControl("Label", {Text = "cvar freeze rets"})
+	panel:AddControl("Label", {Text = "cvar hold ads"})
+	panel:AddControl("Label", {Text = "cvar free aim sc"})
+	
+	panel:AddControl("Label", {Text = "butts wipe|reload"})
+	
+	panel:AddControl("Label", {Text = "sight print nam"})
+	panel:AddControl("Label", {Text = "sight code nam"})
+	panel:AddControl("Label", {Text = "sight cur mdl"})
+	panel:AddControl("Label", {Text = "sight code prefix"})
+	
+	panel:AddControl("Label", {Text = "tentry ??add suffix"})
+	
+	panel:AddControl("Label", {Text = "sight sliders"})
+	
+	panel:AddControl("Label", {Text = "export normal"})
+	panel:AddControl("Label", {Text = "export backup"})
+	panel:AddControl("Label", {Text = "butt export all normal"})
+	panel:AddControl("Label", {Text = "butt export all backup"})
+	
+	panel:AddControl("Label", {Text = "slider zoomamount"})
+	panel:AddControl("Label", {Text = "slider aimvmfov"})
+	panel:AddControl("Label", {Text = "slider aimswayintense"})
 end
 
 function TOOL:SetPanel(panel)
