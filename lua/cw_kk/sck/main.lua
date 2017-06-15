a79cf8adde3be029dd1faed224e766837ff1bb80
Ľubor Slušný <lubor.slusny@gmail.com>
2017-06-15 21:04:22 +0200

    [SCK] unified invalidweapon and notcw2weapon messages, moved more code to client

--- lua/cw_kk/sck/main.lua
+++ lua/cw_kk/sck/main.lua
@@ -12,164 +12,184 @@ BASE.strCCReload = "cw_kk_sck_reload_folder"
 BASE.strCCRebuild = "cw_kk_sck_rebuild_panels"
 BASE.strKnownGLDTKeys = "M203Active|INS2GLActive"
 
-/*
-TOOL table structure
-	[mandatory]
-	Name = "TOOL_NAME"
-	PrintName = "Secksy spawnmenu name"
-	SetPanel(self, panel)
-	
-	[optional]
-	SpawnMenuTab = "Utilities"
-	SpawnMenuTabSection = "Knife Kitty"
-	Initialize(self)
-	OnWeaponChanged(self, new, old)
-	OnWeaponSetupChanged(self)
-	OnWeaponClipChanged(self)
-	OnWeaponGLStateChanged(self)
-	Think(self)
-	
-	[internal]
-	build or update panel - tool shud call from 
-		OnXChanged and SetPanel
-	
-	[pre-defined]
-	SaveSliderZoom(self, slider)
-	LoadSliderZoom(self, slider)
-	AngleToString(Angle)
-	VectorToString(Vector)
-	ThrowNewNotImplemented()
-*/
+if CLIENT then
 
-BASE._toolMeta = {}
-local toolMeta = BASE._toolMeta
-toolMeta.__index = toolMeta
+	/*
+	TOOL table structure
+		[mandatory]
+		Name = "TOOL_NAME"
+		PrintName = "Secksy spawnmenu name"
+		SetPanel(self, panel)
+		
+		[optional]
+		SpawnMenuTab = "Utilities"
+		SpawnMenuTabSection = "Knife Kitty"
+		Initialize(self)
+		OnWeaponChanged(self, new, old)
+		OnWeaponSetupChanged(self)
+		OnWeaponClipChanged(self)
+		OnWeaponGLStateChanged(self)
+		Think(self)
+		
+		[internal]
+		build or update panel - tool shud call from 
+			OnXChanged and SetPanel
+		
+		[pre-defined]
+		SaveSliderZoom(self, slider)
+		LoadSliderZoom(self, slider)
+		AngleToString(Angle)
+		VectorToString(Vector)
+		ThrowNewNotImplemented()
+		ThrowNewInvalidWeapon()
+	*/
 
-function toolMeta:Initialize() end
+	BASE._toolMeta = {}
+	local toolMeta = BASE._toolMeta
+	toolMeta.__index = toolMeta
 
-local function initSliderStorage(tool, slider)
-	if not slider._cwkksckid then
-		local txt = slider.Label:GetText()
-		txt = string.Replace(txt, ".", "_")
-		txt = string.Replace(txt, " ", "_")
-		txt = string.lower(txt)
-		slider._cwkksckid = string.format("_cw_kk_sck_slzoom_%s_%s", tool.Name, txt)
-	end
-	
-	tool._storedSliders = tool._storedSliders or {}
-	
-	if not tool._storedSliders[slider._cwkksckid] then
-		tool._storedSliders[slider._cwkksckid] = CreateClientConVar(slider._cwkksckid, 1, true, false)
-	end
-end
+	function toolMeta:Initialize() end
 
-function toolMeta:SaveSliderZoom(slider)
-	initSliderStorage(self, slider)
-	self._storedSliders[slider._cwkksckid]:SetFloat(slider.Wang:GetZoom())
-end
-
-function toolMeta:LoadSliderZoom(slider)
-	initSliderStorage(self, slider)
-	
-	slider.Wang:SetDecimals(4) // ???
-	slider.Wang:SetZoom(self._storedSliders[slider._cwkksckid]:GetFloat())
-end
-
-function toolMeta:AngleToString(a)
-	if not a then
-		return "nil"
-	end
-	
-	if a:IsZero() then
-		return "Angle()"
-	end
-
-	return string.format(
-		"Angle(%s, %s, %s)",
-		tostring(math.Round(a.p,4)),
-		tostring(math.Round(a.y,4)),
-		tostring(math.Round(a.r,4))
-	)
-end
-
-function toolMeta:VectorToString(v)
-	if not v then
-		return "nil"
-	end
-	
-	if v:Length() == 0 then
-		return "Vector()"
-	end
-
-	return string.format(
-		"Vector(%s, %s, %s)",
-		tostring(math.Round(v.x,4)),
-		tostring(math.Round(v.y,4)),
-		tostring(math.Round(v.z,4))
-	)
-end
-
-function toolMeta:ThrowNewNotImplemented()
-	local snds = {
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/scout_no02.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/pyro_no01.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/soldier_no03.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/demoman_no01.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/heavy_no02.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/medic_no02.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/engineer_no01.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/sniper_no01.mp3",
-		"http://cdn.frustra.org/sounds/sound_tf2/vo/spy_no02.mp3",
-	}
-
-	sound.PlayURL(table.Random(snds), "", function(station)
-		if IsValid(station) then
-			station:Play()
-			print("This stuff aint implemented yet.")
-		else
-			print("INVALID INTERNET CONNECTION OMGF!")
+	local function initSliderStorage(tool, slider)
+		if not slider._cwkksckid then
+			local txt = slider.Label:GetText()
+			txt = string.Replace(txt, ".", "_")
+			txt = string.Replace(txt, " ", "_")
+			txt = string.lower(txt)
+			slider._cwkksckid = string.format("_cw_kk_sck_slzoom_%s_%s", tool.Name, txt)
 		end
-	end)
-end
-
-function BASE:AddTool(tab)
-	if SERVER then return end
-	
-	if not tab then return end
-	if not tab.Name then return end
-	if not tab.SetPanel then return end
-	
-	tab.SpawnMenuTab = tab.SpawnMenuTab or self.SpawnMenuTab
-	tab.SpawnMenuTabSection = tab.SpawnMenuTabSection or self.SpawnMenuTabSection
-	
-	tab.PrintName = tab.PrintName or tab.Name
-	tab.SelectCommand = tab.SelectCommand or ""
-	
-	self._toolCache = self._toolCache or {}
-	
-	setmetatable(tab, self._toolMeta)
-	local old = self._toolCache[tab.Name]
-	
-	if not self._cleanLoad then
-		if old then
-			old.__index = old
-			setmetatable(tab, old)
+		
+		tool._storedSliders = tool._storedSliders or {}
+		
+		if not tool._storedSliders[slider._cwkksckid] then
+			tool._storedSliders[slider._cwkksckid] = CreateClientConVar(slider._cwkksckid, 1, true, false)
 		end
 	end
-	
-	tab:Initialize()
-	
-	self._toolCache[tab.Name] = tab
+
+	function toolMeta:SaveSliderZoom(slider)
+		initSliderStorage(self, slider)
+		self._storedSliders[slider._cwkksckid]:SetFloat(slider.Wang:GetZoom())
+	end
+
+	function toolMeta:LoadSliderZoom(slider)
+		initSliderStorage(self, slider)
+		
+		slider.Wang:SetDecimals(4) // ???
+		slider.Wang:SetZoom(self._storedSliders[slider._cwkksckid]:GetFloat())
+	end
+
+	function toolMeta:AngleToString(a)
+		if not a then
+			return "nil"
+		end
+		
+		if a:IsZero() then
+			return "Angle()"
+		end
+
+		return string.format(
+			"Angle(%s, %s, %s)",
+			tostring(math.Round(a.p,4)),
+			tostring(math.Round(a.y,4)),
+			tostring(math.Round(a.r,4))
+		)
+	end
+
+	function toolMeta:VectorToString(v)
+		if not v then
+			return "nil"
+		end
+		
+		if v:Length() == 0 then
+			return "Vector()"
+		end
+
+		return string.format(
+			"Vector(%s, %s, %s)",
+			tostring(math.Round(v.x,4)),
+			tostring(math.Round(v.y,4)),
+			tostring(math.Round(v.z,4))
+		)
+	end
+
+	function toolMeta:ThrowNewNotImplemented()
+		local snds = {
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/scout_no02.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/pyro_no01.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/soldier_no03.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/demoman_no01.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/heavy_no02.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/medic_no02.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/engineer_no01.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/sniper_no01.mp3",
+			"http://cdn.frustra.org/sounds/sound_tf2/vo/spy_no02.mp3",
+		}
+
+		sound.PlayURL(table.Random(snds), "", function(station)
+			if IsValid(station) then
+				station:Play()
+				print("This stuff aint implemented yet.")
+			else
+				print("INVALID INTERNET CONNECTION OMGF!")
+			end
+		end)
+	end
+
+	function toolMeta:ThrowNewInvalidWeapon()
+		self._panel:ClearControls()
+		self._panel:AddControl("Label", {Text = 
+			"No valid weapon detected. If you reloaded Spawnmenu just now, switch weapons first."
+		})
+	end
+
+	function toolMeta:ThrowNewNotCW2Weapon()
+		self._panel:ClearControls()
+		self._panel:AddControl("Label", {Text = 
+			"Your active weapon is not CW2-based."
+		})
+	end
+
+	function BASE:AddTool(tab)
+		if SERVER then return end
+		
+		if not tab then return end
+		if not tab.Name then return end
+		if not tab.SetPanel then return end
+		
+		tab.SpawnMenuTab = tab.SpawnMenuTab or self.SpawnMenuTab
+		tab.SpawnMenuTabSection = tab.SpawnMenuTabSection or self.SpawnMenuTabSection
+		
+		tab.PrintName = tab.PrintName or tab.Name
+		tab.SelectCommand = tab.SelectCommand or ""
+		
+		self._toolCache = self._toolCache or {}
+		
+		setmetatable(tab, self._toolMeta)
+		local old = self._toolCache[tab.Name]
+		
+		if not self._cleanLoad then
+			if old then
+				old.__index = old
+				setmetatable(tab, old)
+			end
+		end
+		
+		tab:Initialize()
+		
+		self._toolCache[tab.Name] = tab
+	end
 end
 
 function BASE:Load()
-	local reload = self._toolCache != nil
-	
-	self._toolCache = {}
-	self._lastWep = nil
-	self._lastSetup = nil
-	
-	self._knowGLDTkeys = string.Explode("|", self.strKnownGLDTKeys)
+	if CLIENT then
+		local reload = self._toolCache != nil
+		
+		self._toolCache = {}
+		self._lastWep = nil
+		self._lastSetup = nil
+		
+		self._knowGLDTkeys = string.Explode("|", self.strKnownGLDTKeys)
+	end
 	
 	for _,v in pairs(file.Find(self.ToolsFolder .. "*", "LUA")) do
 		AddCSLuaFile(self.ToolsFolder .. v)
@@ -178,8 +198,10 @@ function BASE:Load()
 		end
 	end
 	
-	if reload then
-		RunConsoleCommand("spawnmenu_reload")
+	if CLIENT then
+		if reload then
+			RunConsoleCommand("spawnmenu_reload")
+		end
 	end
 end
 
