2ebd5376f255ff8a19f59b3dccb714f79be04fb5
Ľubor Slušný <lubor.slusny@gmail.com>
2017-07-08 22:16:06 +0200

    EB3 - majority of Create Element view

--- lua/cw_kk/sck/tools/elements.lua
+++ lua/cw_kk/sck/tools/elements.lua
@@ -6,9 +6,9 @@ TOOL.Name = "elements"
 TOOL.PrintName = "Element Browser 3"
 TOOL.Version = "3.0"
 
-TOOL.elementTables = {"AttachmentModelsVM", "AttachmentModelsWM"}
-TOOL.elementProperties = {
-	"model", "pos", "angle", "size", 
+TOOL.elementTables = {
+	"AttachmentModelsVM",
+	"AttachmentModelsWM"
 }
 
 TOOL.headerBackground = Color(0,160,255,100)
@@ -38,8 +38,6 @@ function TOOL:_addSectionGooback()
 		end
 		
 	backgroundPanel:Dock(TOP)
-	backgroundPanel:DockMargin(8,0,8,0)
-	backgroundPanel:SetSize(200,20)
 	backgroundPanel:SetPaintBackground(true)
 	backgroundPanel:SizeToContents()
 end
@@ -61,18 +59,11 @@ function TOOL:_addSectionRefreshButt()
 		end
 		
 	backgroundPanel:Dock(TOP)
-	backgroundPanel:DockMargin(8,0,8,0)
-	backgroundPanel:SetSize(200,20)
 	backgroundPanel:SetPaintBackground(true)
 	backgroundPanel:SizeToContents()
 end
 
-// Element List
-
-local PB = {}
-PB.Name = "list"
-
-function PB:_addSectionETHeader(tableName)
+function TOOL:_addHeaderETName(tableName, fill)
 	local panel = self._panel
 	
 	local backgroundPanel = vgui.Create("DPanel", panel)
@@ -93,17 +84,17 @@ function PB:_addSectionETHeader(tableName)
 		butt:DockMargin(0,4,8,4)
 		
 		function butt:DoClick()
+			TOOL._state.makeElement = fill
 			TOOL:_setBuilder("make")
 		end
 		
 	backgroundPanel:Dock(TOP)
-	backgroundPanel:DockMargin(8,0,8,0)
 	backgroundPanel:SetBackgroundColor(self.headerBackground)
 	backgroundPanel:SetPaintBackground(true)
 	backgroundPanel:SizeToContents()
 end
 
-function PB:_addSectionETItem(tableName, elementName)
+function TOOL:_addHeaderEName(tableName, elementName, reverse)
 	local panel = self._panel
 	local element = self._wep[tableName][elementName]
 	
@@ -111,8 +102,13 @@ function PB:_addSectionETItem(tableName, elementName)
 	panel:AddItem(backgroundPanel)
 		
 		local DoClick = function()
-			TOOL._state.elementID = elementName
-			TOOL:_setBuilder("edit")
+			if !reverse then
+				TOOL._state.editTable = tableName
+				TOOL._state.editElement = elementName
+				TOOL:_setBuilder("edit")
+			else
+				TOOL:_setBuilder("list")
+			end
 		end
 		
 		local Think = function(self)
@@ -124,7 +120,7 @@ function PB:_addSectionETItem(tableName, elementName)
 		end
 		
 		local icon = vgui.Create("DImage", backgroundPanel)
-		icon:SetPos(0,0)
+		icon:SetPos(0,2)
 		icon:SetSize(20,20)
 		icon:SetMouseInputEnabled(true)
 		icon.DoClick = DoClick
@@ -147,7 +143,7 @@ function PB:_addSectionETItem(tableName, elementName)
 		label.Think = Think
 		
 		local label = vgui.Create("DLabel", backgroundPanel)
-		label:SetText("[˅]")
+		label:SetText(reverse and "[˄]" or "[˅]")
 		label:SetDark(true)
 		label:Dock(RIGHT)
 		label:DockMargin(8,0,8,0)
@@ -157,11 +153,14 @@ function PB:_addSectionETItem(tableName, elementName)
 		label.Think = Think
 		
 	backgroundPanel:Dock(TOP)
-	backgroundPanel:DockMargin(8,0,8,0)
-	backgroundPanel:SetSize(200,20)
 	backgroundPanel:SetPaintBackground(true)
 	backgroundPanel:SizeToContents()
 end
+
+// Element List
+
+local PB = {}
+PB.Name = "list"
 
 function PB:run()
 	local panel = self._panel
@@ -169,17 +168,27 @@ function PB:run()
 	local state = self._state
 	
 	for _,tableName in pairs(self.elementTables) do
-		self:_addSectionETHeader(tableName)
+		self:_addHeaderETName(tableName)
 		
 		wep[tableName] = wep[tableName] or {}
+		local aa = wep.ActiveAttachments or {}
 		
 		local sortKeys = {}
 		for elementName,element in pairs(wep[tableName]) do
-			sortKeys[(element.active and "a_" or "b_") .. tostring(elementName)] = elementName
+			// rest
+			local priority = "c_"
+			
+			// active elements
+			priority = element.active and "b_" or priority
+			
+			// elements of active attachment
+			priority = aa[elementName] and "a_" or priority
+			
+			sortKeys[priority .. tostring(elementName)] = elementName
 		end
 		
 		for _,elementName in SortedPairs(sortKeys) do
-			self:_addSectionETItem(tableName, elementName)
+			self:_addHeaderEName(tableName, elementName)
 		end
 	end
 end
@@ -191,12 +200,21 @@ TOOL:addPanelBuilder(PB)
 local PB = {}
 PB.Name = "edit"
 
+PB.elementProperties = {
+	["AttachmentModelsVM"] = {
+		adjustable = true,
+	},
+	["AttachmentModelsWM"] = {},
+}
+
 function PB:run()
 	local panel = self._panel
 	local wep = self._wep
 	local state = self._state
 	
-	panel:AddControl("Label", {Text = "header tableName w/ butt add"})
+	self:_addHeaderETName(state.editTable, state.editElement)
+	self:_addHeaderEName(state.editTable, state.editElement, true)
+	
 	panel:AddControl("Label", {Text = "header elementName w/ butt gooback"})
 	panel:AddControl("Label", {Text = "tickbox active"})
 	panel:AddControl("Label", {Text = "tentry model"})
@@ -215,8 +233,6 @@ function PB:run()
 	panel:AddControl("Label", {Text = "tickbox animated"})
 	panel:AddControl("Label", {Text = "header shortcunts"})
 	panel:AddControl("Label", {Text = "goto/add other element of same name"})
-	
-	self:_addSectionGooback()
 end
 
 TOOL:addPanelBuilder(PB)
@@ -226,28 +242,234 @@ TOOL:addPanelBuilder(PB)
 local PB = {}
 PB.Name = "make"
 
+function PB:_addHeader()
+	local panel = self._panel
+	local wep = self._wep
+	local state = self._state
+	
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+		
+		local label = vgui.Create("DLabel", backgroundPanel)
+		label:SetText("Create new")
+		label:SetDark(true)
+		label:Dock(LEFT)
+		label:DockMargin(8,0,4,0)
+		label:SizeToContents()
+		
+		local butt
+		butt = vgui.Create("DButton", backgroundPanel)
+		butt:SetText("Cancel")
+		butt:Dock(RIGHT)
+		butt:DockMargin(0,4,8,4)
+		
+		function butt:DoClick()
+			TOOL._state.makeElement = nil
+			TOOL:_setBuilder()
+		end
+		
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:SetBackgroundColor(self.headerBackground)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+end
+
+function PB:_addSectionNameEntry()
+	local panel = self._panel
+	local wep = self._wep
+	local state = self._state
+	
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+		
+		local label = vgui.Create("DLabel", backgroundPanel)
+		label:SetText("Element Name:")
+		label:SetDark(true)
+		label:Dock(LEFT)
+		label:DockMargin(8,0,4,0)
+		label:SizeToContents()
+		
+		local entry = vgui.Create("DTextEntry", backgroundPanel)
+		entry:Dock(FILL)
+		entry:DockMargin(4,0,0,0)
+		entry:SetText(state.makeElement)
+		
+		function entry:OnChange()
+			PB._state.makeElement = self:GetValue()
+			PB:_updateCBoxes()
+			PB:_updateButt()
+		end
+		
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+end
+
+function PB:_addSectionModelEntry()
+	local panel = self._panel
+	local wep = self._wep
+	local state = self._state
+	
+	-- panel:AddControl("Label", {Text = "tentry model"})
+	
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+		
+		local label = vgui.Create("DLabel", backgroundPanel)
+		label:SetText("Model:")
+		label:SetDark(true)
+		label:Dock(LEFT)
+		label:DockMargin(8,0,4,0)
+		label:SizeToContents()
+		
+		local entry = vgui.Create("DTextEntry", backgroundPanel)
+		entry:Dock(FILL)
+		entry:DockMargin(4,0,0,0)
+		entry:SetText(state.makeModel)
+		
+		function entry:OnChange()
+			PB._state.makeModel = self:GetValue()
+		end
+		
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+end
+
+function PB:_addSectionETMark(tableName, elementName)
+	local panel = self._panel
+	local wep = self._wep
+	local state = self._state
+	
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+		
+		local cbox = vgui.Create("DCheckBoxLabel", backgroundPanel)
+		cbox:SetDark(true)
+		cbox:Dock(LEFT)
+		cbox:DockMargin(8,0,0,0)
+		
+		cbox.Label:Dock(RIGHT)
+		
+		function cbox:OnChange(val)
+			PB:_updateButt()
+		end
+		
+		self._targets = self._targets or {}
+		self._targets[cbox] = function(self)
+			local elementName = state.makeElement
+			local exists = wep[tableName][elementName] != nil
+			local valid = elementName != ""
+			local can = (!exists) and valid
+			
+			self:SetText(tableName .. (exists and " (already exists)" or ""))
+			self:SetChecked(can)
+			self:SetEnabled(can)
+		end
+		
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+	
+	self:_updateCBoxes()
+end
+
+function PB:_addSectionButtFinish()
+	local panel = self._panel
+	
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+		
+		local butt = vgui.Create("DButton", backgroundPanel)
+		butt:Dock(FILL)
+		butt:SetSize(150,20)
+		butt:SetText("Finish")
+		
+		function butt:DoClick()
+			PB:_finishMaking()
+		end
+		
+		self._finisher = butt
+		
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+	
+	self:_updateButt()
+end
+
+function PB:_updateCBoxes()
+	local t = self._targets
+	for cbox,updateCBox in pairs(t) do
+		if IsValid(cbox) then
+			updateCBox(cbox)
+		else
+			t[cbox] = nil
+		end
+	end
+end
+
+function PB:_updateButt()
+	if !IsValid(self._finisher) then
+		return
+	end
+	
+	local numSelected = #self:_getTargets()
+	
+	self._finisher:SetEnabled(numSelected > 0)
+end
+
+function PB:_getTargets()
+	local t = self._targets
+	local out = {}
+	
+	for cbox,_ in pairs(t) do
+		if IsValid(cbox) then
+			if cbox:GetChecked() then
+				table.insert(out, cbox:GetText())
+			end
+		else
+			t[cbox] = nil
+		end
+	end
+	
+	return out
+end
+
+function PB:_finishMaking()
+	// do stuff
+	// add to elementTable, init
+	
+	PB:ThrowNewNotImplemented()
+	
+	-- PB._state.editTable = targets[1]
+	-- PB._state.editElement = PB._state.makeElement
+	-- PB:_setBuilder("edit")
+end
+
 function PB:run()
 	local panel = self._panel
 	local wep = self._wep
 	local state = self._state
 	
-	panel:AddControl("Label", {Text = "header w/ butt cancer"})
-	panel:AddControl("Label", {Text = "tentry elementName"})
-	panel:AddControl("Label", {Text = "tentry model"})
+	state.makeElement = state.makeElement or "kk_was_here_lol"
+	state.makeModel = state.makeModel or "models/maxofs2d/cube_tool.mdl"
+	
+	self:_addHeader()
+	self:_addSectionNameEntry()
+	self:_addSectionModelEntry()
 	
 	for _,tableName in pairs(self.elementTables) do
-		panel:AddControl("Label", {Text = "tickbox " .. tableName .. " (disabled if exsts)"})
+		self:_addSectionETMark(tableName)
 	end
 	
-	panel:AddControl("Label", {Text = "butt add"})
-	
-	self:_addSectionGooback()
+	self:_addSectionButtFinish()
 end
 
 TOOL:addPanelBuilder(PB)
 
 // Export thing
-
+/*
 local PB = {}
 PB.Name = "expo"
 
@@ -262,7 +484,7 @@ function PB:run()
 end
 
 TOOL:addPanelBuilder(PB)
-
+*/
 // Session manager
 
 function TOOL:Initialize()
@@ -270,7 +492,12 @@ function TOOL:Initialize()
 end
 
 function TOOL:_setBuilder(id)
-	self._state.builderId = id and (self._panelBuilders[id] and id) or "list"
+	id = id or self._statePrevious
+	
+	local new = id and (self._panelBuilders[id] and id) or "list"
+	
+	self._statePrevious = self._state.builderId
+	self._state.builderId = new
 	self:_updatePanel()
 end
 
