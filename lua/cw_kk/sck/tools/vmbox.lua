f0c06c40e44b62ae1f9fcec72370d8080239ff35
Ľubor Slušný <lubor.slusny@gmail.com>
2017-10-19 01:27:47 +0200

    VMBox extended to WM

--- lua/cw_kk/sck/tools/vmbox.lua
+++ lua/cw_kk/sck/tools/vmbox.lua
@@ -8,26 +8,56 @@ TOOL.PrintName = "VM Model"
 function TOOL:_addSectionEntry()
 	local panel = self._panel
 	local wep = self._wep
-	
+
 	local backgroundPanel = vgui.Create("DPanel", panel)
 	panel:AddItem(backgroundPanel)
-		
+
 		local entry = vgui.Create("DTextEntry", backgroundPanel)
 		entry:Dock(FILL)
 		entry:SetText(wep.CW_VM:GetModel())
-		
+
 		function entry:OnEnter()
 			wep.CW_VM:SetModel(self:GetValue())
 		end
-		
+
 		function entry:OnChange()
 			if file.Exists(self:GetValue(), "GAME") then
 				self:OnEnter()
 			end
 		end
-		
+
 		self._entry = entry
-		
+
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:DockMargin(8,0,8,0)
+	backgroundPanel:SetSize(200,20)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+end
+
+function TOOL:_addSectionEntryW()
+	local panel = self._panel
+	local wep = self._wep
+
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+
+		local entry = vgui.Create("DTextEntry", backgroundPanel)
+		entry:Dock(FILL)
+		entry:SetText(wep.WMEnt:GetModel())
+
+		function entry:OnEnter()
+			wep.WMEnt:SetModel(self:GetValue())
+		end
+
+		function entry:OnChange()
+			if file.Exists(self:GetValue(), "GAME") then
+				self:OnEnter()
+			end
+		end
+
+		self._entryW = entry
+
 	backgroundPanel:Dock(TOP)
 	backgroundPanel:DockMargin(8,0,8,0)
 	backgroundPanel:SetSize(200,20)
@@ -38,20 +68,23 @@ end
 function TOOL:_addSectionButt()
 	local panel = self._panel
 	local wep = self._wep
-	
+
 	local backgroundPanel = vgui.Create("DPanel", panel)
 	panel:AddItem(backgroundPanel)
-		
+
 		local butt = vgui.Create("DButton", backgroundPanel)
 		butt:Dock(FILL)
 		butt:SetText("Reset")
-		
+
 		function butt:DoClick()
 			local mdl = wep.ViewModel
 			wep.CW_VM:SetModel(mdl)
 			TOOL._entry:SetText(mdl)
+			local mdl = wep.WorldModel
+			wep.WMEnt:SetModel(mdl)
+			TOOL._entryW:SetText(mdl)
 		end
-		
+
 	backgroundPanel:Dock(TOP)
 	backgroundPanel:DockMargin(8,0,8,0)
 	backgroundPanel:SetSize(200,20)
@@ -62,22 +95,23 @@ end
 function TOOL:_updatePanel()
 	local panel = self._panel
 	local wep = self._wep
-	
+
 	if !IsValid(panel) then return end
-	
+
 	panel:ClearControls()
-	
+
 	if !IsValid(wep) then
 		self:ThrowNewInvalidWeapon()
 		return
 	end
-	
+
 	if !wep.CW20Weapon then
 		self:ThrowNewNotCW2Weapon()
 		return
 	end
-	
+
 	self:_addSectionEntry()
+	self:_addSectionEntryW()
 	self:_addSectionButt()
 end
 
