bb3411666c88ac1702d90a234428302aa8d06201
Ľubor Slušný <lubor.slusny@gmail.com>
2017-10-25 23:13:58 +0200

    updated template code

--- lua/cw_kk/sck/tools/_src/_template.lua
+++ lua/cw_kk/sck/tools/_src/_template.lua
@@ -8,21 +8,21 @@ TOOL.PrintName = "Tool File Template"
 function TOOL:_updatePanel()
 	local panel = self._panel
 	local wep = self._wep
-	
+
 	if !IsValid(panel) then return end
-	
+
 	panel:ClearControls()
-	
+
 	if !IsValid(wep) then
 		self:ThrowNewInvalidWeapon()
 		return
 	end
-	
+
 	if !wep.CW20Weapon then
 		self:ThrowNewNotCW2Weapon()
 		return
 	end
-	
+
 	panel:AddControl("Label", {Text = "2doo"})
 end
 
@@ -38,4 +38,21 @@ end
 
 CustomizableWeaponry_KK.sck:AddTool(TOOL)
 
-TOOL:_updatePanel()
+-- TOOL:_updatePanel() -- only use with SCK loader
+
+/*
+To make this file standalone (not dependent on SCK loader),
+uncomment following and drop file into lua/autorun/client/
+*/
+
+-- MenuTab = "Utilities"
+-- MenuTabSection = "Knife Kitty"
+-- spawnmenu.AddToolMenuOption(
+	-- MenuTab:GetString(),
+	-- MenuTabSection:GetString(),
+	-- "CW_KK_SCK_SA." .. TOOL.name,
+	-- TOOL.PrintName,
+	-- TOOL.SelectCommand or "",
+	-- "",
+	-- function(panel) TOOL:SetPanel(panel) end
+-- )
