8f7761f3b6ecc712946ec5a4b795ac1ae0d07dbc
Ľubor Slušný <lubor.slusny@gmail.com>
2018-04-04 20:03:37 +0200

    AltPos - added mode selection for rest of CW2 origins

--- lua/cw_kk/sck/tools/altpos.lua
+++ lua/cw_kk/sck/tools/altpos.lua
@@ -1,50 +1,25 @@
 AddCSLuaFile()
 
-// some globals
-
-if CLIENT then
-	local cvPB = CreateClientConVar("_cw_kk_pause_blend", 0, false, false)
-
-	if game.SinglePlayer() then
-		local ply, wep
-
-		local origVMMSs = {}
-		local attribute = "performViewmodelMovement"
-		local overrideValue = function(wep)
-			wep.BlendPos = Vector(wep.AlternativePos)
-			wep.BlendAng = Vector(wep.AlternativeAng)
-		end
-		hook.Add("Think", "_cw_kk_sck_lock_ads_think", function()
-			ply = LocalPlayer()
-			wep = ply:GetActiveWeapon()
-
-			if !wep.CW20Weapon then return end
-
-			if cvPB:GetInt() == 1 then
-				origVMMSs[wep] = origVMMSs[wep] or wep[attribute]
-				wep[attribute] = overrideValue
-			else
-				for wep,val in pairs(origVMMSs) do
-					if !IsValid(wep) then
-						continue
-					end
-
-					wep[attribute] = val
-				end
-
-				origVMMSs = {}
-			end
-		end)
-	end
-end
-
 // tool
 
 local TOOL = {}
 
 TOOL.Name = "altpos"
 TOOL.PrintName = "01 | AltPos Builder"
-TOOL.Version = "1.0"
+TOOL.Version = "1.1"
+
+TOOL.suffixes = {"Pos", "Ang"}
+TOOL.modes = {
+	"Alternative",
+	"Customize",
+	"Sprint",
+	"Swim",
+	"Prone",
+}
+
+function TOOL:Initialize()
+	self.prefix = self.prefix or "Alternative"
+end
 
 function TOOL:_addSectionWipeReload()
 	local panel = self._panel
@@ -77,8 +52,8 @@ function TOOL:_addSectionWipeReload()
 
 			// reload
 			if wepStored then
-				for _,part in pairs({"Pos", "Ang"}) do
-					local key = "Alternative" .. part
+				for _,suffix in pairs(TOOL.suffixes) do
+					local key = TOOL.prefix .. suffix
 					local vec = wepStored[key]
 					wep[key] = vec
 				end
@@ -98,8 +73,8 @@ function TOOL:_addSectionSliders()
 	local panel = self._panel
 	local wep = self._wep
 
-	for _,part in pairs({"Pos", "Ang"}) do
-		local key = "Alternative" .. part
+	for _,suffix in pairs(self.suffixes) do
+		local key = self.prefix .. suffix
 		wep[key] =
 			wep[key] and
 			Vector(wep[key]) or
@@ -126,14 +101,14 @@ function TOOL:_addSectionSliders()
 		slider:SetMinMax(s[3], s[4])
 		slider:SetText(s[1] .. "." .. s[2])
 		slider:SetDark(true)
-		slider:SetValue(wep["Alternative" .. s[1]][s[2]])
+		slider:SetValue(wep[self.prefix .. s[1]][s[2]])
 
 		self:LoadSliderZoom(slider)
 
 		function slider:OnValueChanged(val)
 			TOOL:SaveSliderZoom(self)
 
-			local key = "Alternative" .. s[1]
+			local key = TOOL.prefix .. s[1]
 			local vec = wep[key]
 			vec[s[2]] = val
 			wep[key] = vec
@@ -166,36 +141,25 @@ function TOOL:_addSectionPreviewExport()
 	local backgroundPanel = vgui.Create("DPanel", panel)
 	panel:AddItem(backgroundPanel)
 
-		local label
-		label = vgui.Create("DLabel", backgroundPanel)
-		label:SetDark(true)
-		label:Dock(TOP)
-		label:DockMargin(4,4,4,0)
-		label:SizeToContents()
-		label:SetMouseInputEnabled(true)
-		label.DoClick = DoClick
+		for _,suffix in pairs(self.suffixes) do
+			local label
+			label = vgui.Create("DLabel", backgroundPanel)
+			label:SetDark(true)
+			label:Dock(TOP)
+			label:DockMargin(4,4,4,0)
+			label:SizeToContents()
+			label:SetMouseInputEnabled(true)
+			label.DoClick = DoClick
 
-		self._codePreviews[label] = function(l)
-			l:SetText(
-				"SWEP.AlternativePos = " ..
-				self:VectorToString(wep.AlternativePos)
-			)
-		end
+			local key = self.prefix .. suffix
 
-		local label
-		label = vgui.Create("DLabel", backgroundPanel)
-		label:SetDark(true)
-		label:Dock(TOP)
-		label:DockMargin(4,4,4,0)
-		label:SizeToContents()
-		label:SetMouseInputEnabled(true)
-		label.DoClick = DoClick
-
-		self._codePreviews[label] = function(l)
-			l:SetText(
-				"SWEP.AlternativeAng = " ..
-				self:VectorToString(wep.AlternativeAng)
-			)
+			self._codePreviews[label] = function(l)
+				l:SetText(string.format(
+					"SWEP.%s = %s",
+					key,
+					self:VectorToString(wep[key])
+				))
+			end
 		end
 
 	backgroundPanel:Dock(TOP)
@@ -209,6 +173,38 @@ function TOOL:_addSectionPreviewExport()
 	self:_updatePreviews()
 end
 
+function TOOL:_addSectionMode()
+	local panel = self._panel
+	local wep = self._wep
+
+	local backgroundPanel = vgui.Create("DPanel", panel)
+	panel:AddItem(backgroundPanel)
+
+		local box = vgui.Create("DComboBox", backgroundPanel)
+		box:Dock(FILL)
+
+		local function formatPrefix(prefix)
+			return string.format("SWEP.%s*", prefix)
+		end
+
+		box:SetValue(formatPrefix(self.prefix))
+
+		for _,prefix in pairs(self.modes) do
+			box:AddChoice(formatPrefix(prefix), prefix)
+		end
+
+		function box:OnSelect(i, name, data)
+			TOOL.prefix = data
+			TOOL:_updatePanel()
+		end
+
+	backgroundPanel:Dock(TOP)
+	backgroundPanel:DockMargin(8,0,8,0)
+	backgroundPanel:SetSize(200,20)
+	backgroundPanel:SetPaintBackground(true)
+	backgroundPanel:SizeToContents()
+end
+
 function TOOL:_updatePreviews()
 	for label,update in pairs(self._codePreviews) do
 		update(label)
@@ -218,8 +214,8 @@ end
 function TOOL:_getCode()
 	local wep = self._wep
 
-	local pos = "AlternativePos"
-	local ang = "AlternativeAng"
+	local pos = self.prefix .. "Pos"
+	local ang = self.prefix .. "Ang"
 
 	local out = string.format(
 		"\n	SWEP.%s = %s\n	SWEP.%s = %s\n",
@@ -250,8 +246,12 @@ function TOOL:_updatePanel()
 		return
 	end
 
-	local cbox = panel:AddControl("CheckBox", {Label = "Custom weapon origins? (shortcut)", Command = "cw_alternative_vm_pos"})
-	cbox:DockMargin(8,0,8,0)
+	self:_addSectionMode()
+
+	if self.prefix == "Alternative" then
+		local cbox = panel:AddControl("CheckBox", {Label = "Custom weapon origins? (shortcut)", Command = "cw_alternative_vm_pos"})
+		cbox:DockMargin(8,0,8,0)
+	end
 
 	local cbox = panel:AddControl("CheckBox", {Label = "Freeze blend pos", Command = "_cw_kk_pause_blend"})
 	cbox:DockMargin(8,0,8,0)
@@ -281,3 +281,43 @@ end
 CustomizableWeaponry_KK.sck:AddTool(TOOL)
 
 TOOL:_updatePanel()
+
+// some globals
+
+if CLIENT then
+	local cvPB = CreateClientConVar("_cw_kk_pause_blend", 0, false, false)
+
+	if game.SinglePlayer() then
+		local ply, wep
+
+		local origVMMSs = {}
+		local attribute = "performViewmodelMovement"
+
+		hook.Add("Think", "_cw_kk_sck_lock_ads_think", function()
+			ply = LocalPlayer()
+			wep = ply:GetActiveWeapon()
+
+			local overrideValue = function(wep)
+				wep.BlendPos = Vector(wep[TOOL.prefix .. "Pos"])
+				wep.BlendAng = Vector(wep[TOOL.prefix .. "Ang"])
+			end
+
+			if !wep.CW20Weapon then return end
+
+			if cvPB:GetInt() == 1 then
+				origVMMSs[wep] = origVMMSs[wep] or wep[attribute]
+				wep[attribute] = overrideValue
+			else
+				for wep,val in pairs(origVMMSs) do
+					if !IsValid(wep) then
+						continue
+					end
+
+					wep[attribute] = val
+				end
+
+				origVMMSs = {}
+			end
+		end)
+	end
+end
